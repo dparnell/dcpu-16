fcca6ce105b9a5aac03e3238a094dfc821e9db4e
Daniel Parnell <me@danielparnell.com>
2012-04-24 21:53:32 +1000

    Changed the style to use pattern matching via functions rather than in case statements as suggested by Alex Kropivny

--- src/dcpu16_core.erl
+++ src/dcpu16_core.erl
@@ -86,8 +86,10 @@ reg(Cpu, Reg, Value) ->
     end.
 
 
+%% Helper functions for instruction decoding
 decode_read_next_ind(Reg) ->
-    [ nop, 
+    [ 
+      nop, 
       { read_ind, pc },
       { inc, pc },
       { add, Reg },
@@ -95,101 +97,80 @@ decode_read_next_ind(Reg) ->
       { read_reg_ind, pointer }
     ].
 
+reg(2#000)->a;
+reg(2#001)->b;
+reg(2#010)->c;
+reg(2#011)->x;
+reg(2#100)->y;
+reg(2#101)->z;
+reg(2#110)->i;
+reg(2#111)->j.
+
 %% produce the micro operations for a read into the working stack
-decode_read(Source) ->
-    case Source of
-	 0 -> { read_reg, a };
-	 1 -> { read_reg, b };
-	 2 -> { read_reg, c };
-	 3 -> { read_reg, x };
-	 4 -> { read_reg, y };
-	 5 -> { read_reg, z };
-	 6 -> { read_reg, i };
-	 7 -> { read_reg, j };
-	 8 -> { read_ind, a };
-	 9 -> { read_ind, b };
-	10 -> { read_ind, c };
-	11 -> { read_ind, x };
-	12 -> { read_ind, y };
-	13 -> { read_ind, z };
-	14 -> { read_ind, i };
-	15 -> { read_ind, j };
- 	16 -> decode_read_next_ind(a);
-	17 -> decode_read_next_ind(b);
-	18 -> decode_read_next_ind(c);
-	19 -> decode_read_next_ind(x);
-	20 -> decode_read_next_ind(y);
-	21 -> decode_read_next_ind(z);
-	22 -> decode_read_next_ind(i);
-	23 -> decode_read_next_ind(j);
-	24 -> [ { read_reg, sp }, { write_reg, pointer }, { read_ind, sp }, { inc, sp }];  %% POP  == [sp++]
-	25 -> [ { read_reg, sp }, { write_reg, pointer }, { read_ind, sp }];               %% PEEK == [sp]
-	26 -> [ { dec, sp }, { read_reg, sp }, { write_reg, pointer}, { read_ind, sp} ];   %% PUSH == [--sp]
-	27 -> { read_reg, sp };
-	28 -> { read_reg, pc };
-	29 -> { read_reg, overflow };
-	30 -> [ nop, { read_ind, pc }, { inc, pc }, { write_reg, pointer }, { read_ind, pointer } ] ; %% [next word]
-	31 -> [ nop, { read_reg, pc }, { inc, pc }, { write_reg, pointer }, { read_ind, pointer } ] ; %% next word (literal)
-	 _ -> {lit, Source band 31}
-    end.
+decode_read(N) when N=<2#00111 -> { read_reg, reg(N) };
+decode_read(N) when N=<2#01111 -> { read_ind, reg(N bxor 2#01000) };
+decode_read(N) when N=<2#10111 -> decode_read_next_ind(reg(N bxor 2#10000));
+decode_read(2#011000) -> % POP  == [sp++]
+    [ 
+      {read_reg, sp},
+      {write_reg, pointer},
+      {read_ind, sp},
+      {inc, sp}
+    ]; 
+decode_read(2#011001) -> % PEEK == [sp]
+    [ 
+      {read_reg, sp},
+      {write_reg, pointer},
+      {read_ind, sp}
+    ]; 
+decode_read(2#011010) ->  % PUSH == [--sp]
+    [ 
+      {dec, sp},
+      {read_reg, sp},
+      {write_reg, pointer},
+      {read_ind, sp} 
+    ];
+decode_read(2#011011) -> { read_reg, sp };
+decode_read(2#011100) -> { read_reg, pc };
+decode_read(2#011101) -> { read_reg, overflow };
+decode_read(2#011110) -> % [next word]
+    [ 
+      nop,
+      {read_ind, pc},
+      {inc, pc},
+      {write_reg, pointer},
+      {read_ind, pointer}
+    ]; 
+decode_read(2#011111) -> % next word (literal)
+    [ 
+      nop,
+      {read_reg, pc},
+      {inc, pc},
+      {write_reg, pointer},
+      {read_ind, pointer} 
+    ]; 
+decode_read(N) -> {lit, N bxor 2#100000}.
 
 %% produce the micro operations for a write from the working stack
-decode_write(Destination) ->
-    case Destination of
-	0 -> { write_reg, a };
-	1 -> { write_reg, b };
-	2 -> { write_reg, c };
-	3 -> { write_reg, x };
-	4 -> { write_reg, y };
-	5 -> { write_reg, z };
-	6 -> { write_reg, i };
-	7 -> { write_reg, j };
-	8 -> { write_ind, a };
-	9 -> { write_ind, b };
-	10 -> { write_ind, c };
-	11 -> { write_ind, x };
-	12 -> { write_ind, y };
-	13 -> { write_ind, z };
-	14 -> { write_ind, i };
-	15 -> { write_ind, j };
-	16 -> { write_ind, target };
-	17 -> { write_ind, target };
-	18 -> { write_ind, target };
-	19 -> { write_ind, target };
-	20 -> { write_ind, target };
-	21 -> { write_ind, target };
-	22 -> { write_ind, target };
-	23 -> { write_ind, target };
-	24 -> { write_ind, target }; %% POP
-	25 -> { write_ind, target }; %% PEEK
-	26 -> { write_ind, target }; %% PUSH
-	27 -> { write_reg, sp };
-	28 -> { write_reg, pc };
-	29 -> { write_reg, o };
-	30 -> { write_ind, target };
-	31 -> { write_ind, target };
-	_ -> drop
-    end.
+
+decode_write(N) when N=<2#00111 -> { write_reg, reg(N) };
+decode_write(N) when N=<2#01111 -> { write_ind, reg(N bxor 2#01000) };
+decode_write(N) when N=<2#10111 -> { write_ind, target};
+decode_write(2#011000) -> { write_ind, target }; % POP  == [sp++]
+decode_write(2#011001) -> { write_ind, target }; % PEEK == [sp]
+decode_write(2#011010) -> { write_ind, target }; % PUSH == [--sp]
+decode_write(2#011011) -> { write_reg, sp };
+decode_write(2#011100) -> { write_reg, pc };
+decode_write(2#011101) -> { write_reg, overflow };
+decode_write(2#011110) -> { write_ind, target }; % [next word]
+decode_write(2#011111) -> { write_ind, target }; % next word (literal)
+decode_write(_) -> drop.
 
 %% decode a non-basic opcode
 decode_nonbasic_opcode(Opcode, A) ->
     case Opcode of
 	1 -> [decode_read(A), nop, jsr];
 	_ -> [reserved]
-    end.
-
-calculate_pc_usage(Source) ->
-    case Source of
-	16 -> 1;
-	17 -> 1;
-	18 -> 1;
-	19 -> 1;
-	20 -> 1;
-	21 -> 1;
-	22 -> 1;
-	23 -> 1;
-	30 -> 1;
-  	 _ -> 0
     end.
 
 opcode(Op, B) ->
@@ -214,7 +195,41 @@ opcode(Op, B) ->
 	14 -> "IFG";
 	15 -> "IFB"
     end.
-	    
+
+decode_instruction(2#0000, A, B) -> decode_nonbasic_opcode(A, B);
+decode_instruction(2#0001, A, B) -> [decode_read(A), drop, set_target, decode_read(B), nop, decode_write(A)];
+decode_instruction(2#0010, A, B) -> [decode_read(A), set_target, decode_read(B), nop, add, decode_write(A)];
+decode_instruction(2#0011, A, B) -> [decode_read(A), set_target, decode_read(B), nop, sub, decode_write(A)];
+decode_instruction(2#0100, A, B) -> [decode_read(A), set_target, decode_read(B), nop, mul, decode_write(A)];
+decode_instruction(2#0101, A, B) -> [decode_read(A), set_target, decode_read(B), nop, nop, divide, decode_write(A)];
+decode_instruction(2#0110, A, B) -> [decode_read(A), set_target, decode_read(B), nop, nop, mod, decode_write(A)];
+decode_instruction(2#0111, A, B) -> [decode_read(A), set_target, decode_read(B), nop, shl, decode_write(A)];
+decode_instruction(2#1000, A, B) -> [decode_read(A), set_target, decode_read(B), nop, shr, decode_write(A)];
+decode_instruction(2#1001, A, B) -> [decode_read(A), set_target, decode_read(B), logical_and, decode_write(A)];
+decode_instruction(2#1010, A, B) -> [decode_read(A), set_target, decode_read(B), logical_or, decode_write(A)];
+decode_instruction(2#1011, A, B) -> [decode_read(A), set_target, decode_read(B), logical_xor, decode_write(A)];
+decode_instruction(2#1100, A, B) -> [decode_read(A), set_target, decode_read(B), nop, ife];
+decode_instruction(2#1101, A, B) -> [decode_read(A), set_target, decode_read(B), nop, ifn];
+decode_instruction(2#1110, A, B) -> [decode_read(A), set_target, decode_read(B), nop, ifg];
+decode_instruction(2#1111, A, B) -> [decode_read(A), set_target, decode_read(B), nop, ifb].
+
+operand_size(Source) ->
+    case Source of
+	16 -> 1;
+	17 -> 1;
+	18 -> 1;
+	19 -> 1;
+	20 -> 1;
+	21 -> 1;
+	22 -> 1;
+	23 -> 1;
+	30 -> 1;
+  	 _ -> 0
+    end.
+
+instruction_length(0, _, B) -> 1 + operand_size(B);
+instruction_length(_, A, B) -> 1 + operand_size(A) + operand_size(B).
+
 %% When there are no micro operations to perform we need to fetch a new instruction and decode it into micro-operations
 cycle(Cpu, Ram, Cycles, [], CyclesLeft) ->
 %    debug("Cpu = ~p~nRam = ~p~n", [Cpu, Ram]),
@@ -224,135 +239,19 @@ cycle(Cpu, Ram, Cycles, [], CyclesLeft) ->
     debug("~nOpcode = ~p A = ~p B = ~p~n", [opcode(Opcode, B), A, B]),
 
     case Cpu#cpu.skip of
-	false -> Micro_ops = case Opcode of
-				 0 -> decode_nonbasic_opcode(A, B);
-				 1 -> [decode_read(A), drop, set_target, decode_read(B), nop, decode_write(A)];
-				 2 -> [decode_read(A), set_target, decode_read(B), nop, add, decode_write(A)];
-				 3 -> [decode_read(A), set_target, decode_read(B), nop, sub, decode_write(A)];
-				 4 -> [decode_read(A), set_target, decode_read(B), nop, mul, decode_write(A)];
-				 5 -> [decode_read(A), set_target, decode_read(B), nop, nop, divide, decode_write(A)];
-				 6 -> [decode_read(A), set_target, decode_read(B), nop, nop, mod, decode_write(A)];
-				 7 -> [decode_read(A), set_target, decode_read(B), nop, shl, decode_write(A)];
-				 8 -> [decode_read(A), set_target, decode_read(B), nop, shr, decode_write(A)];
-				 9 -> [decode_read(A), set_target, decode_read(B), logical_and, decode_write(A)];
-				 10 -> [decode_read(A), set_target, decode_read(B), logical_or, decode_write(A)];
-				 11 -> [decode_read(A), set_target, decode_read(B), logical_xor, decode_write(A)];
-				 12 -> [decode_read(A), set_target, decode_read(B), nop, ife];
-				 13 -> [decode_read(A), set_target, decode_read(B), nop, ifn];
-				 14 -> [decode_read(A), set_target, decode_read(B), nop, ifg];
-				 15 -> [decode_read(A), set_target, decode_read(B), nop, ifb];
-				 _ -> error
-			     end,
-
+	false -> Micro_ops = decode_instruction(Opcode, A, B),
 %		 debug("Micro ops: ~p~n", [Micro_ops]),
 		 cycle(Cpu#cpu{pc = Cpu#cpu.pc + 1, target = none, pointer = none}, Ram, Cycles, lists:flatten(Micro_ops), CyclesLeft);
-	_ -> %% we want to skip the instruction
-	    case Opcode of
-		0 -> {Cpu#cpu{pc = Cpu#cpu.pc + 1 + calculate_pc_usage(B), skip = false}, Ram, Cycles + 1, []};
-		_ -> {Cpu#cpu{pc = Cpu#cpu.pc + 1 + calculate_pc_usage(A) + calculate_pc_usage(B), skip = false}, Ram, Cycles + 1, []}
-	    end
+	_ -> {Cpu#cpu{pc = Cpu#cpu.pc + instruction_length(Opcode, A, B), skip = false}, Ram, Cycles + 1, []}
     end;		    
-	 
+
+
 %% We have micro operations so we need to perform them
 cycle(Cpu, Ram, Cycles, [Micro_op|Micro_ops], CyclesLeft) ->
 %    debug("~p ~p ~p ~p ~p~n", [Cpu, Ram, Cycles, Micro_op, Micro_ops]),
     debug("~p : ~p~n", [Cycles, Micro_op]),
 
-    { NewCpu, NewRam, Cost } = case Micro_op of
-				   %% internal operations
-
-				   nop -> { Cpu, Ram, 1 };
-				   
-				   free_nop -> { Cpu, Ram, 0 };
-
-				   drop -> [_|T] = Cpu#cpu.w,
-					   { Cpu#cpu{w = T}, Ram, 0};
-				   
-				   dup -> [Value|_] = Cpu#cpu.w,
-					  { Cpu#cpu{w = lists:append([Value], Cpu#cpu.w)}, Ram, 0 };
-
-				   set_target -> Address = Cpu#cpu.pointer,
-						 { Cpu#cpu{target = Address}, Ram, 0 };
-
-				   { add, Reg } -> [V|T] = Cpu#cpu.w,
-						   Result = (V + reg(Cpu, Reg)) band 16#ffff,
-						   { Cpu#cpu{w = lists:append([Result], T)}, Ram, 0 };
-				   
-				   { lit, Value } -> { Cpu#cpu{w = lists:append([Value], Cpu#cpu.w)}, Ram, 0 };
-
-				   { read_reg, Reg } -> { Cpu#cpu{w = lists:append([reg(Cpu, Reg)], Cpu#cpu.w)}, Ram, 0};
-
-				   { write_reg, Reg } -> [Value|T] = Cpu#cpu.w,
-							 Temp = reg(Cpu, Reg, Value),
-							 { Temp#cpu{w = T}, Ram, 0 };
-
-				   { read_ind, Reg } -> Address = reg(Cpu, Reg),
-							Value = array:get(Address, Ram),
-							{ Cpu#cpu{ w = lists:append([Value], Cpu#cpu.w) }, Ram, 0 };
-
-				   { write_ind, Reg } -> Address = reg(Cpu, Reg),
-							 [Value|T] = Cpu#cpu.w,
-							 { Cpu#cpu{ w = T }, array:set(Address, Value, Ram), 0};
-
-
-				   { inc, Reg } -> Value = (reg(Cpu, Reg) + 1) band 16#ffff,
-						   { reg(Cpu, Reg, Value), Ram, 0 };
-
-				   { dec, Reg } -> Value = (reg(Cpu, Reg) - 1) band 16#ffff,
-						   { reg(Cpu, Reg, Value), Ram, 0 };
-
-				   %% aritmetic operations
-				   add -> [B, A] = Cpu#cpu.w,
-					  Sum = A + B,
-					  Overflow = (Sum band 16#10000) bsr 16,
-					  { Cpu#cpu{ w = [Sum band 16#ffff], overflow = Overflow  }, Ram, 1 } ;
-
-				   sub -> [B, A] = Cpu#cpu.w,
-					  Sub = A - B,
-					  Overflow = (Sub band 16#ffff0000) bsr 16,
-					  { Cpu#cpu{ w = [Sub band 16#ffff], overflow = Overflow  }, Ram, 1 };
-				   
-				   mul -> [B, A] = Cpu#cpu.w,
-					  Mul = A * B,
-					  Overflow = (Mul bsr 16) band 16#ffff,
-					  { Cpu#cpu{ w = [Mul band 16#ffff], overflow = Overflow  }, Ram, 1 };
-
-				   divide -> [B, A] = Cpu#cpu.w,
-					     case B of
-						 0 -> { Cpu#cpu{ w = [0], overflow = 0  }, Ram, 1 };
-						 _ -> Div = A div B,
-						      Overflow = ((A bsl 16) div B) band 16#ffff,
-						      { Cpu#cpu{ w = [Div band 16#ffff], overflow = Overflow  }, Ram, 1 }
-					     end;
-
-				   mod -> [B, A] = Cpu#cpu.w,
-					  case B of
-					      0 -> { Cpu#cpu{ w = [0] }, Ram, 1 };
-					      _ -> Mod = A rem B,
-						   { Cpu#cpu{ w = [Mod band 16#ffff] }, Ram, 1 }
-					  end;
-
-				   %% test operations
-				   ifn -> [B, A] = Cpu#cpu.w,
-					  { Cpu#cpu{ w = [], skip = A =:= B }, Ram, 1 };
-
-				   ife -> [B, A] = Cpu#cpu.w,
-					  { Cpu#cpu{ w = [], skip = not(A =:= B) }, Ram, 1 };
-
-				   ifg -> [B, A] = Cpu#cpu.w,
-					  { Cpu#cpu{ w = [], skip = not(A > B) }, Ram, 1 };
-
-				   ifb -> [B, A] = Cpu#cpu.w,
-					  { Cpu#cpu{ w = [], skip = (A band B) =:= 0 }, Ram, 1 };
-
-				   %% extended operations
-				   jsr -> NewSP = (Cpu#cpu.sp - 1) band 16#ffff,
-					  [Address|T] = Cpu#cpu.w,
-					  { Cpu#cpu{ pc = Address, sp = NewSP, w = T}, array:set(NewSP, Cpu#cpu.pc, Ram), 1};
-
-				   reserved -> reserved_dcpu_instruction
-			       end,
-
+    { NewCpu, NewRam, Cost } = execute_micro_op(Micro_op, Cpu, Ram),
     debug("~p~n", [NewCpu]),
 
     case length(Micro_ops) of
@@ -367,6 +266,92 @@ cycle(Cpu, Ram, Cycles, [Micro_op|Micro_ops], CyclesLeft) ->
 		true -> {NewCpu, NewRam, Cycles + Cost, Micro_ops}
 	    end
     end.
+
+%% simple micro operations
+execute_micro_op(nop, Cpu, Ram) ->  { Cpu, Ram, 1 };				   
+execute_micro_op(free_nop, Cpu, Ram) -> { Cpu, Ram, 0 };
+execute_micro_op(drop, Cpu, Ram) -> [_|T] = Cpu#cpu.w,
+				    { Cpu#cpu{w = T}, Ram, 0};
+execute_micro_op(dup, Cpu, Ram) -> [Value|_] = Cpu#cpu.w,
+				   { Cpu#cpu{w = lists:append([Value], Cpu#cpu.w)}, Ram, 0 };
+
+execute_micro_op(set_target, Cpu, Ram) -> Address = Cpu#cpu.pointer,
+					  { Cpu#cpu{target = Address}, Ram, 0 };
+
+execute_micro_op({ add, Reg }, Cpu, Ram) -> [V|T] = Cpu#cpu.w,
+					    Result = (V + reg(Cpu, Reg)) band 16#ffff,
+					    { Cpu#cpu{w = lists:append([Result], T)}, Ram, 0 };
+				   
+execute_micro_op({ lit, Value }, Cpu, Ram) -> { Cpu#cpu{w = lists:append([Value], Cpu#cpu.w)}, Ram, 0 };
+
+execute_micro_op({ read_reg, Reg }, Cpu, Ram) -> { Cpu#cpu{w = lists:append([reg(Cpu, Reg)], Cpu#cpu.w)}, Ram, 0};
+
+execute_micro_op({ write_reg, Reg }, Cpu, Ram) -> [Value|T] = Cpu#cpu.w,
+						  Temp = reg(Cpu, Reg, Value),
+						  { Temp#cpu{w = T}, Ram, 0 };
+
+execute_micro_op({ read_ind, Reg }, Cpu, Ram) -> Address = reg(Cpu, Reg),
+						 Value = array:get(Address, Ram),
+						 { Cpu#cpu{ w = lists:append([Value], Cpu#cpu.w) }, Ram, 0 };
+
+execute_micro_op({ write_ind, Reg }, Cpu, Ram) -> Address = reg(Cpu, Reg),
+						  [Value|T] = Cpu#cpu.w,
+						  { Cpu#cpu{ w = T }, array:set(Address, Value, Ram), 0};
+
+execute_micro_op({ inc, Reg }, Cpu, Ram) -> Value = (reg(Cpu, Reg) + 1) band 16#ffff,
+					    { reg(Cpu, Reg, Value), Ram, 0 };
+
+execute_micro_op({ dec, Reg }, Cpu, Ram) -> Value = (reg(Cpu, Reg) - 1) band 16#ffff,
+					    { reg(Cpu, Reg, Value), Ram, 0 };
+
+%% aritmetic operations
+execute_micro_op(add, Cpu, Ram) -> [B, A] = Cpu#cpu.w,
+				   Sum = A + B,
+				   Overflow = (Sum band 16#10000) bsr 16,
+				   { Cpu#cpu{ w = [Sum band 16#ffff], overflow = Overflow  }, Ram, 1 } ;
+
+execute_micro_op(sub, Cpu, Ram) -> [B, A] = Cpu#cpu.w,
+				   Sub = A - B,
+				   Overflow = (Sub band 16#ffff0000) bsr 16,
+				   { Cpu#cpu{ w = [Sub band 16#ffff], overflow = Overflow  }, Ram, 1 };
+				   
+execute_micro_op(mul, Cpu, Ram) -> [B, A] = Cpu#cpu.w,
+				   Mul = A * B,
+				   Overflow = (Mul bsr 16) band 16#ffff,
+				   { Cpu#cpu{ w = [Mul band 16#ffff], overflow = Overflow  }, Ram, 1 };
+
+execute_micro_op(divide, Cpu, Ram) -> [B, A] = Cpu#cpu.w,
+				      case B of
+					  0 -> { Cpu#cpu{ w = [0], overflow = 0  }, Ram, 1 };
+					  _ -> Div = A div B,
+					       Overflow = ((A bsl 16) div B) band 16#ffff,
+					       { Cpu#cpu{ w = [Div band 16#ffff], overflow = Overflow  }, Ram, 1 }
+				      end;
+
+execute_micro_op(mod, Cpu, Ram) -> [B, A] = Cpu#cpu.w,
+				   case B of
+				       0 -> { Cpu#cpu{ w = [0] }, Ram, 1 };
+				       _ -> Mod = A rem B,
+						   { Cpu#cpu{ w = [Mod band 16#ffff] }, Ram, 1 }
+				   end;
+
+%% test operations
+execute_micro_op(ifn, Cpu, Ram) -> [B, A] = Cpu#cpu.w,
+				   { Cpu#cpu{ w = [], skip = A =:= B }, Ram, 1 };
+
+execute_micro_op(ife, Cpu, Ram) -> [B, A] = Cpu#cpu.w,
+				   { Cpu#cpu{ w = [], skip = not(A =:= B) }, Ram, 1 };
+
+execute_micro_op(ifg, Cpu, Ram) -> [B, A] = Cpu#cpu.w,
+				   { Cpu#cpu{ w = [], skip = not(A > B) }, Ram, 1 };
+
+execute_micro_op(ifb, Cpu, Ram) -> [B, A] = Cpu#cpu.w,
+				   { Cpu#cpu{ w = [], skip = (A band B) =:= 0 }, Ram, 1 };
+
+%% extended operations
+execute_micro_op(jsr, Cpu, Ram) -> NewSP = (Cpu#cpu.sp - 1) band 16#ffff,
+				   [Address|T] = Cpu#cpu.w,
+				   { Cpu#cpu{ pc = Address, sp = NewSP, w = T}, array:set(NewSP, Cpu#cpu.pc, Ram), 1}.
 
 micro_op_cost(Operation) ->
 %    debug("Operation = ~p~n", [Operation]),
