0efe0048b23f0199d82175426cb471b75825ef36
Daniel Parnell <me@danielparnell.com>
2012-05-05 09:23:39 +1000

    Moved the tests into their own directory and updated the makefile to allow for buildint without the tests

--- Makefile
+++ Makefile
@@ -3,8 +3,11 @@ all: test
 compile:
 	erlc -o ebin +debug_info erl_make.erl
 
-test: compile
+dist: compile
 	erl -pa ./ebin -eval "case erl_make:make(development) of ok -> halt(0); _ -> halt(1) end." -s init stop -noshell
+
+test: compile
+	erl -pa ./ebin -eval "case erl_make:make(test) of ok -> halt(0); _ -> halt(1) end." -s init stop -noshell
 
 clean:
 	rm ebin/*.beam
--- erl_make.erl
+++ erl_make.erl
@@ -2,10 +2,16 @@
 
 -export([make/1]).
 
+post_process(test) ->
+    case make:files(filelib:wildcard("test/*.erl"), [debug_info, {outdir, "ebin"}, {i, "include"}]) of
+	error -> error;
+	_ -> test_suite:test()
+    end;
+
+post_process(_) -> ok.
+    
 make(Mode) ->
     case make:all([{d, Mode}]) of
-    	error ->
-    		error;
-    	_ ->
-    		test_suite:test()
+    	error -> error;
+    	_ -> post_process(Mode)
     end.
